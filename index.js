0e7fbd801cdf77d373bb46d09d9e64ec13f66fa6
Miura <tadamiura@gmail.com>
2020-07-22 22:14:14 +0200

    Added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const routes = require('./src/routes/index')
 const { port } = require('./src/helper/service')
+
+app.use(cors())
 
 app.use('/api/about', routes.About)
 app.use('/api/project', routes.Project)
