09e7169955d5f90a0e2d5270eacf89f0bd3e5d40
Miura <tadamiura@gmail.com>
2020-07-22 16:48:01 +0200

    Write request to display in Front side

--- src/routes/about.js
+++ src/routes/about.js
@@ -15,4 +15,29 @@ router.get('/', (req, res) => {
     })
 })
 
+//Get informations for home page
+router.get('/home', (req, res) => {
+    const sql = 'SELECT firstname, lastname, description FROM about_me'
+    connection.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).send('Erreur dans la récupération des information about me pour la home page')
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+//Get informations for about page
+router.get('/presentation', (req, res) => {
+    const sql = 'SELECT about, email, github_profil, linkedin_profil FROM about_me'
+    connection.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).send('Erreur dans la récupération des information about me pour la présentation')
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+
 module.exports = router
--- src/routes/project.js
+++ src/routes/project.js
@@ -2,11 +2,47 @@ const express = require('express')
 const { connection } = require('../helper/conf.js')
 const router = express.Router()
 
+
+// Get les informormations de tous le projets
 router.get('/', (req, res) => {
-    const sql = 'SELECT project_title, project_description, github_link, app_link, date_creation FROM project'
+    const sql = 'SELECT project_title, project_description, github_link, app_link, date_creation, duration FROM project'
     connection.query(sql, (err, result) => {
         if (err) {
             res.status(500).send('Erreur dans la récupération des projects')
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+
+//Get informations about project to display in to My work 
+router.get('/mywork', (req, res) => {
+    const sql = `SELECT project_title, duration, language FROM stack, project, stack_project 
+    WHERE stack.id = id_stack
+    AND project.id = id_project`
+
+
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.log(err)
+            res.status(500).send('Erreur dans la récupération des projects pour la page mywork')
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+// GET informations for each project to display Project page
+router.get('/presentation', (req, res) => {
+    const sql = `SELECT DISTINCT project_title, project_description, date_creation, url_image, github_link, app_link  
+    FROM project, gallery, gallery_project
+    WHERE gallery.id = id_gallery
+    AND project.id = gallery_project.id_project`
+
+    connection.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).send('Erreur dans la récupération des projects pour la page presentation de project')
         } else {
             res.send(result)
         }
